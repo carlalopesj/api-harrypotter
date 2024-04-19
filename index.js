d51763d19fff46fc934b8e2a52a7bbe7da588ee7
Carla Lopes Jovito <carlaljovito@gmail.com>
2024-04-19 12:08:19 -0300

    feat: header

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ import axios from 'axios';
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
 
 
 app.listen(port, () => {
